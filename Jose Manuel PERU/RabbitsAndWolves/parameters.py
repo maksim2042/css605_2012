e1f9f08897f9e8ffa3e2485b65e431428dd6c70b
JoseManuelMAGALLANES <jmagalla@gmu.edu>
2012-12-06 15:11:49 -0500

    rabbits and wolves :)

---
+++ Jose Manuel PERU/RabbitsAndWolves/parameters.py
@@ -0,0 +1,8 @@
+'''
+Created on Dec 6, 2012
+
+@author: josemagallanes
+
+'''
+params = dict(world_shape=(100,100), n_agents=100, maxiter=100, agent_initial_energy=0.0, 
+              extraction_rate=0.1, cell_initial_supply=0.0, cell_max_produce=0.01, agent_max_extract=1.0)
